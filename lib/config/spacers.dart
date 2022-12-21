dc6c41665d8fc735a222fc5d87bf0f8398b9ea69
Dheeraj <dheerajraina55@gmail.com>
2022-12-21 16:03:00 +0530

    Commit 3
    spacers

---
+++ lib/config/spacers.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+Widget vSpacer(height) {
+  return SizedBox(
+    height: height,
+  );
+}
+
+Widget hSpacer(width) {
+  return SizedBox(
+    width: width,
+  );
+}
