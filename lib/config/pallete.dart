e051be408699706e26190dae0565adc20c43b745
Dheeraj <dheerajraina55@gmail.com>
2022-12-21 16:03:27 +0530

    Commit 4
    Custom Pallete

---
+++ lib/config/pallete.dart
@@ -0,0 +1,6 @@
+import "dart:ui";
+class Pallete {
+  final Color lightBlue=Color.fromARGB(255, 187, 217, 236);
+  final Color darkBlue = Color.fromARGB(255, 13, 135, 221);
+  final Color customWhite=Color.fromARGB(255, 255, 255, 255);
+}
